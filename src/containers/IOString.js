68e491f879f6907e4a43e816afd761fe94d87e66
takahiron <odzbdadd1@gmail.com>
2017-12-05 11:16:52 +0900

    Update IOString.js
    delete comment

--- src/containers/IOString.js
+++ src/containers/IOString.js
@@ -4,13 +4,10 @@ import { ioData } from '../actions/action'
 import App from '../components/App'
 
 const mapStateToProps = state => {
-console.log("mapStateToProps")
-console.log(state)
   return state
 }
 
 const mapDispatchToProps = dispatch => {
-console.log("dispatch")
   return {
     onClickAdd: text => {dispatch(ioData(text))}
   }
