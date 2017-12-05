57c401ddee81fd0f205660c5153e1e73e3b30a2c
takahiron <odzbdadd1@gmail.com>
2017-12-05 11:17:18 +0900

    Update index.js
    delete comment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,8 @@
-console.log("reduser/index")
 export default function reducer(state, action){
   switch(action.type){
     case 'ADD':
-console.log("reduser/index ADD")
-console.log(state)
-console.log(action)
       return {no: action.no, text: action.text}
     default:
-console.log("reduser/index default")
       return {no: 0, text: "start"}
   }
 }
